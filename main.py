669a2f815aad6595e05cad6aeac71614f3f8ab3a
Francisco Ortega <francisco.ortega@live.fr>
2018-03-19 19:51:16 +0100

    Paramètres

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ import tkinter as tk
 
 ####### MAIN SCRIPT - BE PSO ##############
 
+#Parameters
+
+canvas_width = 1000
+canvas_heigh = 800
+cities = 7
+
+
 #Création du canvas
 
 master = tk.Tk()
 
-view = 
+view = Canvas(master, width = canvas_width, height = canvas_heigh)
 
 
 #Création population
